054782535aa90c451e9a544f8a579fe2ac268cac
rahman sanuth <sanuthrahman@gmail.com>
2022-08-19 14:31:02 +0100

    level 6 add google signin

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLM = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 
 const app = express();
 
@@ -36,28 +39,65 @@ mongoose.connect(process.env.DB, {useNewUrlparser: true});
 
  const UserSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String 
  })
 
 
 //  UserSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 UserSchema.plugin(passportLM);
+UserSchema.plugin(findOrCreate);
 
 
  const User = mongoose.model("User",UserSchema)
 
  passport.use(User.createStrategy());
 
- passport.serializeUser(User.serializeUser());
- passport.deserializeUser(User.deserializeUser());
- 
+ passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
 
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+ 
+ passport.use(new GoogleStrategy({
+  clientID: process.env.CID,
+  clientSecret: process.env.CST,
+  callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+console.log('url under callback ');
 
 
 app.get("/", function(req, res){
     res.render('home')
     });
+
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+  
+app.get('/auth/google/secrets', passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/secrets');
+    });
+  
 
 
 
